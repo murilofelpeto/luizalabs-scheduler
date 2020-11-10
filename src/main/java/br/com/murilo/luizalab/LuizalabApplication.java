b69daebe7f982d1102c7c3f200c6f76b508ba286
murilofelpeto <murilofelpeto@gmail.com>
2020-11-10 17:55:56 -0300

    First commit

---
+++ src/main/java/br/com/murilo/luizalab/LuizalabApplication.java
@@ -0,0 +1,13 @@
+package br.com.murilo.luizalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizalabApplication.class, args);
+	}
+
+}
